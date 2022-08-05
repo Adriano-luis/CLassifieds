c53b85d9ceb475ac5c7bb1d1f0deae1c99c5585d
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-08-05 12:20:43 +0100

    basic structure

---
+++ pages/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classifieds</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand">Classifieds</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right mr-auto">
+                <li class="nav-item">
+                    <a href="" class="nav-link">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Login</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a href="" class="nav-link">My items</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Logout</a>
+                </li> -->
+            </ul>
+        </div>
+    </nav>
